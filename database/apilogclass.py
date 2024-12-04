faac6a8eea9bf2d24b17a2640d8a2fb21f020cbc
mpshmakov <zeras4z4@gmail.com>
2024-12-04 18:18:25 +0000

    added retention to apilogs class

--- database/apilogclass.py
+++ database/apilogclass.py
@@ -1,15 +1,18 @@
 from database.schema import ApiLogs
 from uuid import uuid4
-from database.operations import insertRow
+from database.operations import deleteApiLogsRowsOlderThanRetentionPolicy, insertRow
 
 class apilog:
     def info(self, username:str, message:str):
+        deleteApiLogsRowsOlderThanRetentionPolicy()
         insertRow(ApiLogs(str(uuid4()), username, "info", message))
     
     def warning(self, username:str, message:str):
+        deleteApiLogsRowsOlderThanRetentionPolicy()
         insertRow(ApiLogs(str(uuid4()), username, "warning", message))
 
     def error(self, username:str, message:str):
+        deleteApiLogsRowsOlderThanRetentionPolicy()
         insertRow(ApiLogs(str(uuid4()), username, "error", message))
 
 log = apilog()
